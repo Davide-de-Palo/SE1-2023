71cb44d33eb9f6f3210071088ea597fde05a563b
Davide-de-Palo <davide.palo@smail.inf.h-brs.de>
2023-10-12 12:30:26 +0200

    Test verfollständigt.

--- src/uebung1/test/GermanTranslatorTest.java
+++ src/uebung1/test/GermanTranslatorTest.java
@@ -1,17 +1,34 @@
 package uebung1.test;
 
-import uebung1.control.GermanTranslator;
 import org.junit.jupiter.api.Test;
+import uebung1.control.GermanTranslator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GermanTranslatorTest {
 
     @Test
-    void aPositiveTest() {
+     void aPositiveTest() {
         GermanTranslator translator = new GermanTranslator();
-        String value = translator.translateNumber(3);
-        assertEquals(value, "drei");
+        String value = translator.translateNumber(5);
+        assertEquals(value, "fünf");
     }
+    @Test
+    void aIndexoutOfBounceTest() {
+        GermanTranslator translator = new GermanTranslator();
+        String value = translator.translateNumber(11);
+        assertEquals(value, "Übersetzung der Zahl 11 nicht möglich (1.0)");
+    }
+    @Test
 
+    void aNegativTest() {
+        GermanTranslator translator = new GermanTranslator();
+        String value = translator.translateNumber(-1);
+        assertEquals(value, "Übersetzung der Zahl -1 nicht möglich (1.0)");
+    }
+    void aNegativ2Test() {
+        GermanTranslator translator = new GermanTranslator();
+        String value = translator.translateNumber(-8);
+        assertEquals(value, "Übersetzung der Zahl -8 nicht möglich (1.0)");
+    }
 }
